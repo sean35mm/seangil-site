a9215c15ae1856b13a5c663e003f8fd4c992618d
sean35mm <sean35mm@gmail.com>
2022-10-19 20:49:48 -0700

    new temp

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import WindowMockup from '../components/WindowMockup/WindowMockup';
+import About from '../components/About/About';
+
+export default function Home() {
+	return (
+		<div className='flex justify-center flex-col items-center'>
+			<h1>hello world!</h1>
+		</div>
+	);
+}
