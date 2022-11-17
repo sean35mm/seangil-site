d4441f7781588e08d54be61210a8acbe3d002298
sean35mm <sean35mm@gmail.com>
2022-11-16 23:11:24 -0800

    having ide issues

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import GithubBanner from '../components/GithubBanner';
 
 export default function Home() {
 	return (
-		<main className='flex justify-center items-center h-screen bg-black'>
+		<main className='flex justify-center items-center min-h-screen bg-black'>
 			<div className='mockup-window border border-black bg-secondary m-4'>
 				<div className='sm:flex px-4 py-16 bg-base-100 justify-evenly'>
 					<About />
