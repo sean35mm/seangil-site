f558ea56d7b58f981e63561a03c1d5759d50f4e8
sean35mm <sean35mm@gmail.com>
2022-12-07 22:13:37 -0800

    hero layout done - add animation later

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Hero = () => {
 	return (
-		<section className='w-11/12 max-h-[703px] bg-secondary text-black rounded-2xl'>
-			<div className='flex justify-around p-32 h-full'>
+		<section className='w-11/12 h-[703px] bg-secondary text-black rounded-2xl'>
+			<div className='md:flex justify-around p-8 md:p-24 lg:p-32 h-full'>
 				<h1 className='text-9xl flex items-start'>Hello,</h1>
 				<div className='flex items-end'>
-					<div>
-						<h3 className='text-xl'>I&apos;m Sean Gil</h3>
+					<div className='mt-10 md:m-0'>
+						<h3 className='text-md'>I&apos;m</h3>
+            <h3 className='text-2xl'>Sean Gil</h3>
 						<h3>
 							/ sh-aW-n · g<i>il</i> /
 						</h3>
@@ -18,13 +19,13 @@ const Hero = () => {
 								1. Software Engineer, full stack with a knack for the front end.
 							</li>
               <li>
-                2. US Army Veteran and former Senior VP of a due diligence firm
+                2. US Army Veteran and former Senior VP in Financial Investigations
               </li>
               <li>
                 3. Currently teaching web development basics at BrainStation
               </li>
               <li>
-                4. _______
+                4. ___
               </li>
 						</ol>
 					</div>
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,11 @@ import Hero from "../components/Hero";
 
 export default function Home() {
 	return (
-		<main className="bg-black h-screen w-full flex justify-center">
-			{/* <div className='prose'>
-				<h1 className="text-lime">
-					Re-vamping my website - checkout the progress on the development
-					branch on{' '}
-					<a className="text-secondary" href='https://github.com/sean35mm/seangil-site/tree/develop'>
-						Github
-					</a>
-					!
-				</h1>
-			</div> */}
-
-			<Hero />
+		<main className="bg-black h-screen w-full">
+			<div className="flex justify-center">
+				<Hero />
+			</div>
+			
 		</main>
 	);
 }
