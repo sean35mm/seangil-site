d9951a4470399bf8e01474e0db55881efcac8107
sean g14 <sean35mm@gmail.com>
2023-05-19 11:26:59 -0700

    done

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+	url: string;
+	children: React.ReactNode;
+};
 
-const CustomLink = (props: Props) => {
-	return <a>CustomLink</a>;
+export const CustomLink = (props: Props) => {
+	return (
+		<a
+			target='_blank'
+			rel='noopener noreferrer'
+			href={props.url}
+			className='text-lime underline underline-offset-2'
+		>
+			{props.children}
+		</a>
+	);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,28 @@
+import { CustomLink } from '../components/CustomLink';
+import Image from 'next/image';
+import resume from '../assets/resume.svg';
+import { FaFileDownload } from 'react-icons/fa';
+
 export default function Home() {
 	return (
-		<main className='bg-black grid h-screen place-items-center'>
+		<main className='bg-black flex justify-center items-center h-screen'>
 			<div className='mx-auto w-3/4 text-secondary tracking-wider'>
 				<h1>
 					Hi, I&apos;m{' '}
-					<a
-						href='https://read.cv/seangil'
-						className='text-lime underline underline-offset-2'
-					>
-						Sean
-					</a>
-					, a software engineer based in Los Angeles, CA and soon Austin, TX.
+					<CustomLink url='https://read.cv/seangil'>Sean</CustomLink>, a
+					software engineer based in Los Angeles, CA and Austin, TX.
 				</h1>
 				<h2>
-					Currently helping web dev become accessible for all at BrainStation.
+					Currently helping web dev become accessible for all at{' '}
+					<CustomLink url='https://brainstation.io'>BrainStation</CustomLink>.
 				</h2>
+				<div className='flex flex-col items-center mt-10'>
+					<h3 className='flex mb-2 items-center'>
+						<FaFileDownload className='mr-1' />
+						Resume
+					</h3>
+					<Image src={resume} alt='resume' width={100} height={100} />
+				</div>
 			</div>
 		</main>
 	);
