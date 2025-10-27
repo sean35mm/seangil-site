ac292e184581ad671a7dec463bea64b432af1526
sean <77409500+sean35mm@users.noreply.github.com>
2025-10-26 20:50:52 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,45 @@
-import Link from 'next/link'
-import './globals.css'
-import { Nunito_Sans } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
-import { Analytics } from '@/components/analytics'
-import { ModeToggle } from '@/components/mode-toggle'
+import Link from 'next/link';
+import './globals.css';
+import { Nunito_Sans } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@/components/analytics';
+import { ModeToggle } from '@/components/mode-toggle';
 
-const font = Nunito_Sans({ subsets: ['latin'] })
+const font = Nunito_Sans({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Sean Gil',
-	description: "Sean Gil's personal website and blog",
-}
+  title: 'Sean Gil',
+  description: "Sean Gil's personal website and blog",
+};
 
 interface RootLayoutProps {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-	return (
-		<html lang='en'>
-			<body
-				className={`antialiased min-h-screen bg-white dark:bg-black text-slate-900 dark:text-white ${font.className}`}
-			>
-				<ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
-					<div className='max-w-2xl mx-auto py-10 px-4'>
-						<header>
-							<div className='flex items-center justify-between'>
-								<ModeToggle />
-								<nav className='ml-auto text-sm font-medium space-x-6'>
-									<Link href='/'>Home</Link>
-									<Link href='/blog'>Blog</Link>
-								</nav>
-							</div>
-						</header>
-						<main className='flex justify-center items-center mt-10'>
-							{children}
-						</main>
-					</div>
-					<Analytics />
-				</ThemeProvider>
-			</body>
-		</html>
-	)
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={`antialiased min-h-screen bg-white dark:bg-black text-slate-900 dark:text-white ${font.className}`}
+      >
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+          <div className='max-w-2xl mx-auto py-10 px-4'>
+            <header>
+              <div className='flex items-center justify-between'>
+                <ModeToggle />
+                <nav className='ml-auto text-sm font-medium space-x-6'>
+                  <Link href='/'>Home</Link>
+                  <Link href='/blog'>Blog</Link>
+                </nav>
+              </div>
+            </header>
+            <main className='flex justify-center items-center mt-10'>
+              {children}
+            </main>
+          </div>
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,54 +1,54 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
 
-/** @type {import('contentlayer/source-files').ComputedFields} */
+/** @type {import('contentlayer2/source-files').ComputedFields} */
 const computedFields = {
-	slug: {
-		type: 'string',
-		resolve: (doc) => `/${doc._raw.flattenedPath}`,
-	},
-	slugAsParams: {
-		type: 'string',
-		resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
-	},
-}
+  slug: {
+    type: 'string',
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
+  },
+};
 
 export const Page = defineDocumentType(() => ({
-	name: 'Page',
-	filePathPattern: `pages/**/*.mdx`,
-	contentType: 'mdx',
-	fields: {
-		title: {
-			type: 'string',
-			required: true,
-		},
-		description: {
-			type: 'string',
-		},
-	},
-	computedFields,
-}))
+  name: 'Page',
+  filePathPattern: `pages/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+    },
+  },
+  computedFields,
+}));
 
 export const Post = defineDocumentType(() => ({
-	name: 'Post',
-	filePathPattern: `blog/**/*.mdx`,
-	contentType: 'mdx',
-	fields: {
-		title: {
-			type: 'string',
-			required: true,
-		},
-		description: {
-			type: 'string',
-		},
-		date: {
-			type: 'date',
-			required: true,
-		},
-	},
-	computedFields,
-}))
+  name: 'Post',
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+  },
+  computedFields,
+}));
 
 export default makeSource({
-	contentDirPath: './content',
-	documentTypes: [Post, Page],
-})
+  contentDirPath: './content',
+  documentTypes: [Post, Page],
+});
